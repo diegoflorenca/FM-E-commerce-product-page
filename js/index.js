f700b993a931971ff8e1564fd8c8df766769d078
Diego Felipe Florença <diegoflorenca@gmail.com>
2021-11-22 15:22:21 -0300

    Header mobile styled

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
+/*
+Menu toggle
+*/
+const btnMenu = document.querySelector('.menu-toggle');
+const headerElement = document.querySelector('header');
+
+btnMenu.addEventListener('click', menuToggle);
+
+function menuToggle() {
+  headerElement.classList.toggle('active');
+}
+
+/*
+Product quantity selector
+*/
 const btnMinus = document.querySelector('[data-quantity=minus]');
 const btnPlus = document.querySelector('[data-quantity=plus]');
 const inputQuantity = document.querySelector('#input-quantity');
@@ -8,11 +23,11 @@ btnPlus.addEventListener('click', updateQuantity);
 function updateQuantity(e) {
   const operation = e.currentTarget.dataset.quantity;
 
-  if(operation == 'minus' && inputQuantity.value != 0) {
+  if (operation == 'minus' && inputQuantity.value != 0) {
     inputQuantity.value--;
   }
 
-  if(operation == 'plus') {
+  if (operation == 'plus') {
     inputQuantity.value++;
   }
 }
